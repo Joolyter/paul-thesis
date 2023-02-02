ae528766dfc332dac796fc638f20bc1c7d13a998
Paul Schmiedtke <paul-schmiedtke@live.com>
2023-02-02 07:19:56 +0100

    no message

--- PythonInput/src/setup.py
+++ PythonInput/src/setup.py
@@ -9,8 +9,8 @@ setup(
     long_description="Joolyter was developed in the course of Bachelor thesis "
                 "'A Gamification Approach to the Teaching of Space Flight Mechanics'. "
                 "It represents the core application, interpreting solution scripts to use kRPC "
-                "to communicate with KSP."
-                "Source code: https://github.com/Joolyter/paul-thesis/tree/main/PythonInput/src"
+                "to communicate with KSP. \n\n"
+                "Source code: https://github.com/Joolyter/paul-thesis/tree/main/PythonInput/src \n"
                 "Documentation: https://joolyter.github.io/paul-thesis/PythonInput/src/docs/_build/html/index.html",
     author="Paul J. Schmiedtke",
     license="None",
